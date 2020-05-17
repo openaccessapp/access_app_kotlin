5f7a5c4a9d65a1713dbefd86358187169e40fbcd
deyanm <deiandeni13@gmail.com>
2020-05-17 23:14:43 +0300

    Initial

---
+++ app/src/main/java/dahoum/wales/access_app/MainActivity.kt
@@ -0,0 +1,16 @@
+package dahoum.wales.access_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun viki() : Boolean {
+        return true;
+    }
+}
