3d4775714c16d7fbc27bd2a8427c1cca29b5975a
deyanm <deiandeni13@gmail.com>
2020-05-18 01:16:21 +0300

    Changed

--- app/src/main/java/dahoum/wales/access_app/MainActivity.kt
+++ app/src/main/java/dahoum/wales/access_app/MainActivity.kt
@@ -2,15 +2,21 @@ package dahoum.wales.access_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    fun viki() : Boolean {
-        return true;
+        val linearLayout: LinearLayout = findViewById(R.id.buttonsLayout);
+        for (i in 0 until linearLayout.childCount) {
+            val child = linearLayout.getChildAt(i)
+            child.setOnClickListener({
+                print("tapped")
+            })
+        }
     }
 }
