d710dce8f9483c57a9b1832b4b569bcd16754947
deyanm <deiandeni13@gmail.com>
2020-05-18 11:59:17 +0300

    Buttons

--- app/src/main/java/dahoum/wales/access_app/MainActivity.kt
+++ app/src/main/java/dahoum/wales/access_app/MainActivity.kt
@@ -1,21 +1,31 @@
 package dahoum.wales.access_app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.widget.LinearLayout
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG: String? = MainActivity::class.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val linearLayout: LinearLayout = findViewById(R.id.buttonsLayout);
-        for (i in 0 until linearLayout.childCount) {
-            val child = linearLayout.getChildAt(i)
-            child.setOnClickListener({
-                print("tapped")
-            })
+        val buttons: List<Button> = listOf(button1, button2, button3)
+
+        for (button in buttons) {
+            button.setOnClickListener {
+                val color: Int = (button.background as ColorDrawable).color
+                if (color == ContextCompat.getColor(this, R.color.red)) {
+                    button.setBackgroundColor(ContextCompat.getColor(this, R.color.green))
+                } else {
+                    button.setBackgroundColor(ContextCompat.getColor(this, R.color.red))
+                }
+            }
         }
     }
 }
