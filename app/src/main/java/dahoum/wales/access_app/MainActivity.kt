667e73996b9ca7ebab839d8d8769392be4132301
nasicha94 <62597273+nasicha94@users.noreply.github.com>
2020-05-18 11:09:40 +0300

    Added SecondActivity basic design for the project.

--- app/src/main/java/dahoum/wales/access_app/MainActivity.kt
+++ app/src/main/java/dahoum/wales/access_app/MainActivity.kt
@@ -2,7 +2,6 @@ package dahoum.wales.access_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
---
+++ app/src/main/java/dahoum/wales/access_app/SecondActivity.kt
@@ -0,0 +1,14 @@
+package dahoum.wales.access_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.LinearLayout
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+    }
+}
