5e487de0cc8aa2f373c3792b269c1d12f630e4eb
nasicha94 <62597273+nasicha94@users.noreply.github.com>
2020-05-20 22:36:16 +0300

    Date/Time

--- app/src/main/java/dahoum/wales/access_app/MainActivity2.java
+++ app/src/main/java/dahoum/wales/access_app/MainActivity2.java
@@ -8,13 +8,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.gson.JsonObject;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.BreakIterator;
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity2<RequestQueue, JsonObjectRequest> extends AppCompatActivity {
 
     private static final String TAG = MainActivity2.class.getSimpleName();
 
@@ -36,6 +46,58 @@ public class MainActivity2 extends AppCompatActivity {
                 Toast.makeText(MainActivity2.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    public static void POSTRequest() throws IOException {
+
+        final String POST_PARAMS = "{\n" + "\"userId\": 101,\r\n" +
+
+                "    \"id\": 101,\r\n" +
+
+                "    \"title\": \"Test Title\",\r\n" +
+
+                "    \"body\": \"Test Body\"" + "\n}";
+
+        System.out.println(POST_PARAMS);
+
+        URL obj = new URL("https://jsonplaceholder.typicode.com/posts");
+
+        HttpURLConnection postConnection = (HttpURLConnection) obj.openConnection();
+        postConnection.setRequestMethod("POST");
+        postConnection.setRequestProperty("userId", "a1bcdefgh");
+        postConnection.setRequestProperty("Content-Type", "application/json");
+        postConnection.setDoOutput(true);
+
+        OutputStream os = postConnection.getOutputStream();
+        os.write(POST_PARAMS.getBytes());
+        os.flush();
+        os.close();
+
+        int responseCode = postConnection.getResponseCode();
+
+        System.out.println("POST Response Code :  " + responseCode);
+
+        System.out.println("POST Response Message : " + postConnection.getResponseMessage());
+
+        if (responseCode == HttpURLConnection.HTTP_CREATED) {
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+
+                    postConnection.getInputStream()));
+
+            String inputLine;
+
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in .readLine()) != null) {
+                response.append(inputLine);
+            } in .close();
+            System.out.println(response.toString());
+        } else {
+            System.out.println("POST NOT WORKED");
+
+        }
+
     }
 }
 
--- app/src/main/java/dahoum/wales/access_app/RetrofitService.java
+++ app/src/main/java/dahoum/wales/access_app/RetrofitService.java
@@ -6,9 +6,13 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface RetrofitService {
 
     @GET("/posts")
     Call<List<JsonObject>> getAllPosts();
+
+    @POST("/posts")
+    Call<List<JsonObject>> POSTRequest();
 }
