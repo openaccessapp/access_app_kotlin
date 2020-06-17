027804ed8eafa36d77f0e82c6eafe12f32b4bb96
deyanm <deiandeni13@gmail.com>
2020-06-17 11:43:14 +0300

    Onboarding screen Save button fixed added name

--- app/src/main/java/dahoum/wales/access_app/SplashActivity.java
+++ app/src/main/java/dahoum/wales/access_app/SplashActivity.java
@@ -2,21 +2,43 @@ package dahoum.wales.access_app;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.Transformation;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 public class SplashActivity extends AppCompatActivity {
 
+    SectionsPagerAdapter mSectionsPagerAdapter;
+
+    private ViewPager mViewPager;
+
+    ImageView zero, one, two;
+    ImageView[] indicators;
+
+    int lastLeftValue = 0;
+
+    int page = 0;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.black_trans80));
+
         setContentView(R.layout.activity_splash);
 
         ImageView logo = findViewById(R.id.imageView);
@@ -25,23 +47,6 @@ public class SplashActivity extends AppCompatActivity {
             startActivity(new Intent(this, MainActivity2.class));
             finish();
         });
-//        float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 300, getResources().getDisplayMetrics());
-//        ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) logo.getLayoutParams();
-//        final int bottomStartMargin = params.bottomMargin;
-//        final int bottomEndMargin = (int) px;
-//        Animation anim = new Animation() {
-//            @Override
-//            protected void applyTransformation(float interpolatedTime, Transformation t) {
-//                ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) logo.getLayoutParams();
-//                params.bottomMargin = bottomStartMargin + (int) ((bottomEndMargin - bottomStartMargin) * interpolatedTime);
-//                logo.setLayoutParams(params);
-//                findViewById(R.id.mainTv).setVisibility(View.VISIBLE);
-//                findViewById(R.id.descTv).setVisibility(View.VISIBLE);
-//                getStarted.setVisibility(View.VISIBLE);
-//            }
-//        };
-//        anim.setDuration(500);
-//        logo.startAnimation(anim);
         ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) logo.getLayoutParams();
         final float startBias = params.verticalBias;
         final float endBias = 0.3f;
@@ -56,7 +61,6 @@ public class SplashActivity extends AppCompatActivity {
         anim.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
-                findViewById(R.id.descTv).setVisibility(View.VISIBLE);
                 getStarted.setVisibility(View.VISIBLE);
             }
 
@@ -72,5 +76,142 @@ public class SplashActivity extends AppCompatActivity {
         });
         anim.setDuration(500);
         logo.startAnimation(anim);
+
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        zero = (ImageView) findViewById(R.id.intro_indicator_0);
+        one = (ImageView) findViewById(R.id.intro_indicator_1);
+        two = (ImageView) findViewById(R.id.intro_indicator_2);
+
+
+        indicators = new ImageView[]{zero, one, two};
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        mViewPager.setCurrentItem(page);
+        updateIndicators(page);
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+                page = position;
+
+                updateIndicators(page);
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+    }
+
+    void updateIndicators(int position) {
+        for (int i = 0; i < indicators.length; i++) {
+            indicators[i].setBackgroundTintList(
+                    i == position ? ContextCompat.getColorStateList(this, R.color.text_gray) : ContextCompat.getColorStateList(this, R.color.disabled_tint)
+            );
+        }
+    }
+
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        ImageView img;
+
+//        int[] bgs = new int[]{R.drawable.ic_flight_24dp, R.drawable.ic_mail_24dp, R.drawable.ic_explore_24dp};
+
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_pager, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.descTv);
+            switch (getArguments().getInt(ARG_SECTION_NUMBER)) {
+                case 1:
+                    textView.setText("Safe and queue free visiting.");
+                    break;
+                case 2:
+                    textView.setText("Fast and easy planning.");
+                    break;
+                case 3:
+                    break;
+            }
+
+            img = (ImageView) rootView.findViewById(R.id.section_img);
+            img.setBackgroundResource(R.drawable.ic_guarantee);
+
+
+            return rootView;
+        }
+
+
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "SECTION 1";
+                case 1:
+                    return "SECTION 2";
+                case 2:
+                    return "SECTION 3";
+            }
+            return null;
+        }
+
     }
 }
